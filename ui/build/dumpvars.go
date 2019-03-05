741b0e3700a5a3441c103eb8950aaa0f6c5364b5
spezi77 <spezi7713@gmx.net>
2019-03-05 23:39:28 +0100

    dumpvars: Show the sign key information.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -87,6 +87,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 // Variables to print out in the top banner
 var BannerVars = []string{
 	"PIXELDUST_VERSION",
+	"SIGN_KEY",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_ARCH",
 	"TARGET_ARCH_VARIANT",
